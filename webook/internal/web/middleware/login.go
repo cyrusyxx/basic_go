d364ca4d18f1a0215f498a13f4c16aafdb6d37b4
cyrus <584124633@qq.com>
2024-01-09 21:43:36 +0800

    完成session刷新

--- webook/internal/web/middleware/login.go
+++ webook/internal/web/middleware/login.go
@@ -1,9 +1,11 @@
 package middleware
 
 import (
+	"encoding/gob"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"time"
 )
 
 type LoginMiddlewareBuilder struct {
@@ -11,14 +13,28 @@ type LoginMiddlewareBuilder struct {
 
 func (m LoginMiddlewareBuilder) CheckLogin() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		gob.Register(time.Now())
+
 		path := ctx.Request.URL.Path
 		if path == "/users/signup" || path == "/users/login" {
 			return
 		}
 		sess := sessions.Default(ctx)
-		if sess.Get("userId") == nil {
+		uid := sess.Get("userId")
+		if uid == nil {
 			ctx.AbortWithStatus(http.StatusUnauthorized)
 			return
+		}
+
+		const updateTimeKey = "update_time"
+		utime, ok := sess.Get(updateTimeKey).(time.Time)
+		if !ok || time.Now().Sub(utime) > time.Minute {
+			sess.Set(updateTimeKey, time.Now())
+			sess.Set("userId", uid)
+			err := sess.Save()
+			if err != nil {
+
+			}
 		}
 	}
 }
