5f909dba5d950739c3c2c7111c4ea44c025f0897
cyrus <584124633@qq.com>
2024-03-11 19:46:14 +0800

    Finish Web User Test

---
+++ webook/internal/web/user_test.go
@@ -0,0 +1,101 @@
+package web
+
+import (
+	"bytes"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"webook/webook/internal/domain"
+	"webook/webook/internal/service"
+	svcmocks "webook/webook/internal/service/mocks"
+)
+
+func TestUserHandler_SignUp(t *testing.T) {
+	testCases := []struct {
+		name string
+
+		mock func(ctrl *gomock.Controller) (service.UserService, service.CodeService)
+
+		reqBuilder func(t *testing.T) *http.Request
+
+		wantCode int
+		wantBody string
+	}{
+		{
+			name: "success",
+			mock: func(ctrl *gomock.Controller) (service.UserService, service.CodeService) {
+				userService := svcmocks.NewMockUserService(ctrl)
+				userService.EXPECT().Signup(gomock.Any(), domain.User{
+					Email:    "12345@qq.com",
+					Password: "hello#world123",
+				}).Return(nil)
+
+				codeService := svcmocks.NewMockCodeService(ctrl)
+
+				return userService, codeService
+			},
+			reqBuilder: func(t *testing.T) *http.Request {
+				req, err := http.NewRequest(http.MethodPost,
+					"/users/signup", bytes.NewReader([]byte(`{
+"email":"12345@qq.com",
+"password":"hello#world123",
+"confirmPassword":"hello#world123"
+}`)))
+				req.Header.Set("Content-Type", "application/json")
+				assert.NoError(t, err)
+
+				return req
+			},
+			wantCode: http.StatusOK,
+			wantBody: "HELLO ITS IN SIGNUP",
+		},
+
+		{
+			name: "Email Pattern is wrong",
+			mock: func(ctrl *gomock.Controller) (service.UserService, service.CodeService) {
+				userService := svcmocks.NewMockUserService(ctrl)
+				codeService := svcmocks.NewMockCodeService(ctrl)
+
+				return userService, codeService
+			},
+			reqBuilder: func(t *testing.T) *http.Request {
+				req, err := http.NewRequest(http.MethodPost,
+					"/users/signup", bytes.NewReader([]byte(`{
+"email":"12345",
+"password":"hello#world123",
+"confirmPassword":"hello#world123"
+}`)))
+				req.Header.Set("Content-Type", "application/json")
+				assert.NoError(t, err)
+
+				return req
+			},
+			wantCode: http.StatusOK,
+			wantBody: "Email pattern is wrong\n",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			userService, codeService := tc.mock(ctrl)
+			hdl := NewUserHandler(userService, codeService)
+
+			server := gin.Default()
+			hdl.RegisterRoutes(server)
+
+			req := tc.reqBuilder(t)
+			recorder := httptest.NewRecorder()
+
+			server.ServeHTTP(recorder, req)
+
+			assert.Equal(t, tc.wantCode, recorder.Code)
+			assert.Equal(t, tc.wantBody, recorder.Body.String())
+		})
+	}
+}
