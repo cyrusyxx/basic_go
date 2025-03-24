d3c50af5ca3cede43677933a48c5e3a6b087127a
cyruschang <cyruschang@xiaoe-tech.com>
2025-03-24 16:23:07 +0800

    fix 修复article model

--- webook/internal/repository/dao/article.go
+++ webook/internal/repository/dao/article.go
@@ -10,9 +10,9 @@ import (
 )
 
 type Article struct {
-	Id       int64  `gorm:"primaryKey,autoIncrement" bson:"id,omitempty"`
-	Title    string `gorm:"type=varchar(4096)" bson:"title,omitempty"`
-	Content  string `gorm:"type=BLOB" bson:"content,omitempty"`
+	Id       int64  `gorm:"primaryKey;autoIncrement" bson:"id,omitempty"`
+	Title    string `gorm:"type:varchar(4096)" bson:"title,omitempty"`
+	Content  string `gorm:"type:longtext" bson:"content,omitempty"`
 	AuthorId int64  `gorm:"index" bson:"author_id,omitempty"`
 	Status   uint8  `bson:"status,omitempty"`
 
